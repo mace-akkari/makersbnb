bcb7f8b0599ace49b010fed88acdfefc220804fd
Gonzalo Berhouet <gonzaloberhouet@Gonzalos-Air.fritz.box>
2020-10-27 16:32:09 +0000

    spaces display and add new spaces

--- lib/app.rb
+++ lib/app.rb
@@ -1,4 +1,5 @@
 require "sinatra/base"
 require "sinatra/activerecord"
 require_relative "models/user"
+require_relative "models/space"
 require_relative "controllers/makersbnb"
--- lib/controllers/makersbnb.rb
+++ lib/controllers/makersbnb.rb
@@ -1,5 +1,32 @@
+require 'pg'
+require_relative './../database_connection'
+require_relative './../models/space.rb'
+
 class MakersBNB < Sinatra::Base
+  set :root, File.dirname(File.expand_path("..", __FILE__))
+  enable :sessions
+  session_secret
+  set :session_secret, "ENV"
+  # register Sinatra::Flash
+  
   get "/" do
-    "Hello world"
+    erb :index
   end
+
+  get "/spaces" do
+    # @username = User.find(session[:])
+    @spaces = Space.all
+    erb :spaces
+  end
+
+  post "/spaces" do
+    Space.create(description: params[:description], location: params[:location], availability: params[:availability])
+    redirect "/spaces"
+  end
+
+  get "/spaces/new" do
+    erb :form
+  end
+
+
 end
