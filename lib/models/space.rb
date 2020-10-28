e6b2a31604c07b4f868fb4c8ff8add853e7c49ef
Dan Holmes <dh@danielholmes.com>
2020-10-28 15:20:36 +0000

    refactor email method

--- lib/models/space.rb
+++ lib/models/space.rb
@@ -1,9 +1,15 @@
 class Space < ActiveRecord::Base
   belongs_to :users
 
+  def email
+    users = User.joins("INNER JOIN spaces ON spaces.user_id = users.id AND spaces.id = '#{id}'")
+    user = users.first
+    user.email
+  end
+
   def self.fetch_email
     @spaces = Space.all
-    @spaces.each do | space |
+    @spaces.each do |space|
       User.joins("INNER JOIN spaces ON spaces.user_id = users.id AND spaces.id = '#{space.id}'").first
 
       # user = User.find("#{space.user_id}")
