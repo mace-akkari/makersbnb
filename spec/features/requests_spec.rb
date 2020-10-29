889a3ae92447481e16610f4b5c5e30e74c406c81
Dan Holmes <dh@danielholmes.com>
2020-10-29 10:21:22 +0000

    changes to request test

--- spec/features/requests_spec.rb
+++ spec/features/requests_spec.rb
@@ -7,15 +7,13 @@ feature "requests" do
     #actions
     login
     find("1").click_button "Rent me"
-    fill_in "start_date", with: "01/01/2021"
-    fill_in "end_date", with: "17/01/2021"
+    fill_in "date", with: "01/01/2021"
     click_button "Submit"
-    # Request create
+    # Request created
     click_link "Log out"
     login_second_user
     visit "/requests"
     expect(page).to have_content "Big house"
     expect(page).to have_content "01/01/2021"
-    expect(page).to have_content "17/01/2021"
   end
 end
