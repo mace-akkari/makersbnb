7d40751234e2f58c60e23c237d816a837750621b
Otis <otisvickersgraver@gmail.com>
2020-10-29 14:34:49 +0000

    spaces_spec test improvement

--- spec/features/spaces_spec.rb
+++ spec/features/spaces_spec.rb
@@ -3,28 +3,23 @@ feature "Spaces for rent" do
     create_user
     create_space
   end
-  scenario "Welcoming message" do
-    visit "/spaces"
-    expect(page).to have_content "Spaces available"
-  end
-
   scenario "display spaces for rent" do
     visit "/spaces"
-    expect(page).to have_content "Space description:"
+    expect(page).to have_content "Space description: nice flat"
   end
 
   scenario "display location of the space for rent" do
     visit "/spaces"
-    expect(page).to have_content "Location:"
+    expect(page).to have_content "Location: London"
   end
 
   scenario "display price of the space for rent" do
     visit "/spaces"
-    expect(page).to have_content "Price:"
+    expect(page).to have_content "Price per night: 100"
   end
 
-  scenario "displey email address" do
+  scenario "display email address" do
     visit "/spaces"
-    expect(page).to have_content "Email:"
+    expect(page).to have_content "Email: otisvickersgraver@gmail.com"
   end
 end
